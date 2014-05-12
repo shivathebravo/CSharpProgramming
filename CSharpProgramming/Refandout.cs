e3265038a01bc83e9c2ca3b27ea853c39d7a5380
shivathebravo <shivathebravo@gmail.com>
2014-05-11 22:38:56 -0400

    About Ref and out

--- CSharpProgramming/Refandout.cs
+++ CSharpProgramming/Refandout.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace CSharpProgramming
 {
+  /// <summary>
+  ///  Ref cause an argument to be passed by reference  but must be initialized to the source objecct
+  ///  Out cause an argument to be passed bt reference but do not need to be initialized
+  /// </summary>
   public  class Refandout
     {
+      
     }
 }
